a70041d2808d66893ecb07aab0b5acf0306697a1
Yuliya Tokareva <ta4tonado@gmail.com>
2023-04-07 15:11:34 +0300

    test

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const webpack = require('webpack');
 const path = require('path');
-const Dotenv = require('dotenv-webpack');
 
 module.exports = (env, argv) => {
   const isProduction = argv.mode === 'production';
